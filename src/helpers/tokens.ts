d8600aaa20c185401ea793d757596b4265d68f44
Mayank Mittal <msquare.pvt21@gmail.com>
2024-05-07 20:32:57 +0530

    Amount Fix

--- src/helpers/tokens.ts
+++ src/helpers/tokens.ts
@@ -3,19 +3,19 @@ const tokens = [
         name: 'USDC',
         address: '0x28c3d1cd466ba22f6cae51b1a4692a831696391a',
         decimals: 6,
-        min_amount: 10,
+        min_amount: 9,
     },
     {
         name: 'USDT',
         address: '0x68c9736781e9316ebf5c3d49fe0c1f45d2d104cd',
         decimals: 6,
-        min_amount: 10,
+        min_amount: 9,
     },
     {
         name: 'WETH',
         address: '0x5622f6dc93e08a8b717b149677930c38d5d50682',
         decimals: 18,
-        min_amount: 0.0025,
+        min_amount: 0.002,
     },
 ]
 
